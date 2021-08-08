0a8994d3addfd4d517413d663f9cabeb2a08ac21
agustin-montiveros <agus.monti14@outlook.com>
2021-08-07 23:34:28 -0300

    Ejercicio1 terminado

---
+++ tp0ej1.c
@@ -0,0 +1,18 @@
+/*************************
+Ingenieria en Computación - UNRN Andina
+Programación 1
+minombredeusuario de GitHub
+*************************/
+#include <stdio.h>
+
+  /* Para compilar el programa usamos el comando gcc tp0ej1.c 
+	 Se puede usar al final del comando la instruccion -o para darle el nombre que querramos.
+  */
+  
+int main(int argc, char *argv[])
+{
+	printf("Hola mundo");
+	return 0;
+}
+
+
